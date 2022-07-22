bb936303e7a3082d46055fd9dcb4a3c29281e467
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-07-22 15:02:35 +0100

    Updated app title

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,9 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1395, 772);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Invoicing";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
